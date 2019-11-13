ff5bf7b43d83fdac4347c2d0020e1b8efef96ad8
Robin Lallier <robin.lallier@tuni.fi>
2019-11-13 14:50:32 +0200

    Updates

---
+++ precache-manifest.94047380a93a7c8558f4c7310a3c24b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52de943bceba94c16cd9e78957399041",
+    "url": "/kieran-globe40/index.html"
+  },
+  {
+    "revision": "1f5f21e6b5c3913bf482",
+    "url": "/kieran-globe40/static/css/main.4afea211.chunk.css"
+  },
+  {
+    "revision": "5fdbee53869ef692052f",
+    "url": "/kieran-globe40/static/js/2.3b92c5aa.chunk.js"
+  },
+  {
+    "revision": "1f5f21e6b5c3913bf482",
+    "url": "/kieran-globe40/static/js/main.95e604c6.chunk.js"
+  },
+  {
+    "revision": "9720549db2e22f1b6b3d",
+    "url": "/kieran-globe40/static/js/runtime-main.e306e53c.js"
+  },
+  {
+    "revision": "e8785f8b81cd67537a7d891048cf95ea",
+    "url": "/kieran-globe40/static/media/boat.e8785f8b.png"
+  },
+  {
+    "revision": "fe81d91da315a0621cb39db35c8d685d",
+    "url": "/kieran-globe40/static/media/boat_icon.fe81d91d.svg"
+  },
+  {
+    "revision": "df74992688562d81c283a9d839ab5cf4",
+    "url": "/kieran-globe40/static/media/kieran.df749926.png"
+  },
+  {
+    "revision": "5b52cbdf5a39d2b41224465f42ad3161",
+    "url": "/kieran-globe40/static/media/race_background.5b52cbdf.png"
+  }
+]);
